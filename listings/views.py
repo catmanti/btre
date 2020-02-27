b31485550ee36bf2ce28887a6f8784e7b36f4020
catman <somaratne@yahoo.com>
2020-02-27 06:12:20 +0530

    after listing page

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -7,12 +7,10 @@ def index(request):
     listings = Listing.objects.order_by(
         '-list_date').filter(is_published=True)
 
-    #----
     paginator = Paginator(listings, 6)
 
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
-    #----
 
     context = {
         'listings': paged_listings
@@ -22,7 +20,13 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
